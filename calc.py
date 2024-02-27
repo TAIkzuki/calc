5a7be53558661c6754881de3c3ddb49a4356723b
kzuki <kzuki@yandex.ru>
2024-02-27 21:59:38 +0300

    v01.0.1

--- calc.py
+++ calc.py
@@ -1,1 +1,32 @@
-print("hello")
+import tkinter
+
+
+class Calculations:
+    global a
+    global b
+    def __new__(cls, *args, **kwargs):
+        print()
+    def plus(self):
+        return a + b
+
+    def minus(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        if b == 0:
+            print("in '0' not delete")
+        else:
+            return a / b
+
+    def procent(self, a, b):
+        return (a / b) * 100
+
+    def root(self, a, b):
+        pass
+
+Calculations.a= int(input('a'))
+Calculations.b=int(input('b'))
+print(Calculations.division(se,Calculations.a,Calculations.b))
---
+++ tkinterr.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+def say_hello():
+    print("hello")
+
+def counter():
+    global count
+    count+=1
+    btn3['text']=f
+count = 0
+win = tk.Tk()
+win.title('calculate')
+#photo = tk.PhotoImage(file='tt.png')
+#win.iconphoto(False, photo)
+win.config(bg='#2DFAFF')
+label_1 = tk.Label(win,text='v 1.0.0')
+label_1.pack()
+btn1=tk.Button(win,text=range(1,10,1),command=say_hello)
+btn2=tk.Button(win,text="lambda",command=lambda :tk.Label(win,text='new lmbd').pack())
+btn3=tk.Button(win,text=f'счетчик: {count}'
+               ,command=counter()
+               ,
+               )
+
+
+btn1.pack()
+btn2.pack()
+
+h=500
+w=600
+win.geometry(f"{h}x{w}")
+#win.minsize(300,400)
+win.resizable(False,False)
+win.mainloop()
